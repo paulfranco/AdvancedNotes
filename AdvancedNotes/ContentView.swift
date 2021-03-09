33e80d7f61cdb34a735a5c0e938164b9b9097048
Paul Franco <mail@paulfran.co>
2021-03-09 02:01:27 -0800

    Initial Commit

---
+++ AdvancedNotes/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  AdvancedNotes
+//
+//  Created by Paul Franco on 3/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
