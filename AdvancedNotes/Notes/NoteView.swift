c9f5e239ac07e173a8862d49ef20b9db0cdb5fc7
Paul Franco <mail@paulfran.co>
2021-03-10 21:53:32 -0800

    save image in CoreData

--- AdvancedNotes/Notes/NoteView.swift
+++ AdvancedNotes/Notes/NoteView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct NoteView: View {
     
     @ObservedObject var note: Note
+    @State private var isDropTargeted: Bool = false
     
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
@@ -29,11 +30,39 @@ struct NoteView: View {
             //TextEditor(text: $note.bodyText)
             TextViewWrapper(note: note)
             
+            if note.img != nil {
+                Image(nsImage: NSImage(data: note.img!) ?? NSImage())
+                    .resizable()
+                    .scaledToFit()
+                    .contextMenu(ContextMenu(menuItems: {
+                        Button(action: {
+                            note.img = nil
+                        }, label: {
+                            Text("Remove Image")
+                        })
+                    }))
+            }
+            
             Text("Keywords:")
             
             Text("Linked Notes: ")
         }.padding()
+        .background(isDropTargeted ? Color.gray : Color.clear)
+        .onDrop(of: ["public.file-url"], isTargeted: $isDropTargeted, perform: { providers in
+            handleDrop(providers: providers)
+        })
         
+    }
+    
+    func handleDrop(providers: [NSItemProvider]) -> Bool {
+        let found = providers.loadObjects(ofType: URL.self) { item in
+            if let image = NSImage(contentsOf: item.absoluteURL) {
+                let data = image.tiffRepresentation
+                note.img = data
+                
+            }
+        }
+        return found
     }
 }
 
